560236b3616d675ebc51a45511be3b17fcc9852e
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-11-28 15:43:17 +0100

    fix:joiValidator

--- ape_backend/src/joiValidator/registerValidator.middleware.js
+++ ape_backend/src/joiValidator/registerValidator.middleware.js
@@ -5,55 +5,35 @@ export const registerValidatorSchema = Joi.object({
     firstname: Joi.string()
         .min(2)
         .max(64)
-        .pattern(new RegExp('^[A-Za-z]+$'))
-        .required()
-        .messages({
-            'string.min': 'Le prénom doit contenir au moins 2 caractères.',
-            'string.max': 'Le prénom ne peut pas dépasser 64 caractères.',
-            'string.pattern.base': 'Le prénom ne peut contenir que des lettres.',
-            'any.required': 'Le prénom est obligatoire.'
-        }),
+        .pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ-' ]+$/)
+        .required(),
 
     lastname: Joi.string()
         .min(2)
         .max(64)
-        .pattern(new RegExp('^[A-Za-z]+$'))
-        .required()
-        .messages({
-            'string.min': 'Le nom doit contenir au moins 2 caractères.',
-            'string.max': 'Le nom ne peut pas dépasser 64 caractères.',
-            'string.pattern.base': 'Le nom ne peut contenir que des lettres.',
-            'any.required': 'Le nom est obligatoire.'
-        }),
+        .pattern(/^[A-Za-zÀ-ÖØ-öø-ÿ-' ]+$/)
+        .required(),
 
     email: Joi.string()
-        .email({ minDomainSegments: 2 })
-        .required()
-        .messages({
-            'string.email': 'L\'email doit être valide.',
-            'any.required': 'L\'email est obligatoire.'
-        }),
+        .email()
+        .required(),
+
     password: Joi.string()
         .min(8)
-        .pattern(new RegExp('(?=.*[a-z])'))
-        .pattern(new RegExp('(?=.*[A-Z])'))
-        .pattern(new RegExp('(?=.*[0-9])'))
-        .pattern(new RegExp('(?=.*[!@#\$%\^&\*])'))
+        .pattern(/(?=.*[a-z])/)
+        .pattern(/(?=.*[A-Z])/)
+        .pattern(/(?=.*[0-9])/)
+        .pattern(/(?=.*[!@#$%^&*])/)
+        .required(),
+
+    repeat_password: Joi.string()
+        .valid(Joi.ref("password"))
         .required()
-        .optional()
         .messages({
-            'string.min': 'Le mot de passe doit contenir au moins 8 caractères.',
-            'string.pattern.base': 'Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial.',
-            'any.required': 'Le mot de passe est obligatoire.'
+            "any.only": "Les mots de passe doivent correspondre."
         }),
 
-    repeat_password: Joi.any().valid(Joi.ref("password"))
-        .optional()
-        .required()
-        .messages({
-            'any.only': 'Les mots de passe doivent correspondre.',
-            'any.required': 'La confirmation du mot de passe est obligatoire.'
-        }),
+    role_id: Joi.number().required()
 });
 
 export const validateRegister = validate(registerValidatorSchema);
