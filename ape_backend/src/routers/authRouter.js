f81ed36741d86d809838c535a35d32baff44ee75
Ludo-dev-1 <thibaud.ludovic@sfr.fr>
2025-11-28 15:19:09 +0100

    chore:joiValidator in router

--- ape_backend/src/routers/authRouter.js
+++ ape_backend/src/routers/authRouter.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import authController from "../controllers/authController.js";
 import { authenticate } from "../authenticate/auth.js";
+import { validateRegister } from "../joiValidator/registerValidator.middleware.js";
 
 const authRouter = Router();
 
 // Routes pour l'authentification
 authRouter.post("/login", authController.login);
-authRouter.post("/register", authController.register);
+authRouter.post("/register", validateRegister, authController.register);
 authRouter.get("/profile", authenticate, authController.getProfile);
 authRouter.post("/change-password", authenticate, authController.changePassword);
 
